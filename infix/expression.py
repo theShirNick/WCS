22e46e8b843455f80a3282c2632f1a3a170ea5e3
Nikolai Shiriaev <theShirNick@gmail.com>
2021-04-24 20:32:23 +0200

    tilde fix

--- infix/expression.py
+++ infix/expression.py
@@ -6,7 +6,7 @@ from typing import Any, Iterator
 from atom import Atom
 class InfixExpression:
     def __init__(self, expression: str, atoms: dict[str, Atom]) -> None:
-        self.expression = expression.replace('&', '∧').replace('^', '∧').replace('˜', '¬').replace('!', '¬').replace('|', '∨')
+        self.expression = expression.replace('&', '∧').replace('^', '∧').replace('~', '¬').replace('!', '¬').replace('|', '∨')
         self.atoms = atoms
         self.__parse_lexer_tokens(self.__get_lexer_tokens())
 
