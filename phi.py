277633769989759a79d0b69c0556a7d9884894ce
Nikolai Shiriaev <theShirNick@gmail.com>
2021-05-07 15:39:42 +0200

    skipping steps phi fix

--- phi.py
+++ phi.py
@@ -6,6 +6,7 @@ def phi(program:Program, interpretation:Interpretation) -> Interpretation:
 
     new_interpretation = interpretation.clone()
     candidates_for_false = dict()
+    to_true = set()
     # get immediate consequences
     for clause in program.clauses:
         if len(clause.body.atoms_here) == 1:
@@ -13,16 +14,11 @@ def phi(program:Program, interpretation:Interpretation) -> Interpretation:
             clause.body.atoms_here.add(consequence_atom) # pop then add. looks hacky
 
             if clause.head.evaluate() == TruthConstant.TRUE:
-                consequence_atom.ground_value = TruthConstant.TRUE
-                if consequence_atom in new_interpretation.unknowns:
-                    new_interpretation.unknowns.remove(consequence_atom)
-                    new_interpretation.trues.add(consequence_atom)
-                if consequence_atom in new_interpretation.falses:
-                    new_interpretation.falses.remove(consequence_atom)
-                    new_interpretation.trues.add(consequence_atom)
-                
+                to_true.add(consequence_atom)
+                                
                 if consequence_atom in candidates_for_false:
                     candidates_for_false[consequence_atom] = False
+
             elif clause.head.evaluate() == TruthConstant.FALSE:
                 if consequence_atom not in candidates_for_false:
                     candidates_for_false[consequence_atom] = True
@@ -31,7 +27,16 @@ def phi(program:Program, interpretation:Interpretation) -> Interpretation:
                     candidates_for_false[consequence_atom] = False
         else:
             raise Exception(f'Could not get immediate consequence for {clause}. There must be 1 atom in the body; but found {len(clause.body.atoms_here)}.')
-            
+
+    for atom in to_true:
+        atom.ground_value = TruthConstant.TRUE
+        if atom in new_interpretation.unknowns:
+            new_interpretation.unknowns.remove(atom)
+            new_interpretation.trues.add(atom)
+        if atom in new_interpretation.falses:
+            new_interpretation.falses.remove(atom)
+            new_interpretation.trues.add(atom)
+        
     for atom in candidates_for_false:
         if candidates_for_false[atom] == True:
             atom.ground_value = TruthConstant.FALSE
